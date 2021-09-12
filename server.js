60f406b2f6b8bd4d56931f061724d123d9d38b02
Micheal Limones <Micheal.Limones@du.edu>
2021-09-11 18:36:39 -0600

    added database connection to server.

--- server.js
+++ server.js
@@ -8,6 +8,16 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: employee_db,
+  },
+  console.log('COnnected to employee_db database!')
+);
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
