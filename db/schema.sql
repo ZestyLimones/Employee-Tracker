4b8fa86847e1c9eb42f95b22626935b202057fe0
Micheal Limones <Micheal.Limones@du.edu>
2021-09-13 09:10:22 -0600

    added in get requests for departments, roles, and employees.

--- db/schema.sql
+++ db/schema.sql
@@ -17,7 +17,7 @@ CREATE TABLE role (
 );
 
 CREATE TABLE employee (
-     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logo = require('asciiart-logo');
 const mysql = require('mysql2');
 const dotenv = require('dotenv').config();
 
-const PORT = process.env.PORT || 30001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -19,6 +19,51 @@ const db = mysql.createConnection(
   },
   console.log(`Connected to the employee_db database.`)
 );
+
+app.get('/api/departments', (req, res) => {
+  const sql = `SELECT id, name AS "department name" FROM department`;
+
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success!',
+      data: rows,
+    });
+  });
+});
+
+app.get('/api/roles', (req, res) => {
+  const sql = `SELECT id, title, salary, department_id AS "department id" FROM role`;
+
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success!',
+      data: rows,
+    });
+  });
+});
+
+app.get('/api/employees', (req, res) => {
+  const sql = `SELECT id, first_name AS "first name", last_name AS "last name", role_id AS "role id", manager_id AS "manager id" FROM employee`;
+
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success!',
+      data: rows,
+    });
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
